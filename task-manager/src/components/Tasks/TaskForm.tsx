2655def9c160a5d2ba29d26aecfb4d5d2217b113
stanleyavril123 <stanley.avril@outlook.com>
2024-11-14 20:57:26 -0500

    Task object structure defined

--- task-manager/src/components/Tasks/TaskForm.tsx
+++ task-manager/src/components/Tasks/TaskForm.tsx
@@ -6,6 +6,26 @@ const TaskForm = () => {
     const [description, setDescription] = useState('');
     const [dueDate, setDueDate] = useState('');
     const [priority, setPriority] = useState('low');
-    const [status, setStatus] = useState('not started'); // maybe bool
+    const [status, setStatus] = useState('not started');
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const Task = {
+            title,
+            description,
+            dueDate,
+            priority,
+            status
+        };
+
+    };
+
+
+
+    return (
+        <form>
+        </form>
+    );
 }
 export default TaskForm;
--- task-manager/src/pages/Home.tsx
+++ task-manager/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SideBars from '../components/Sidebar/SideBars.tsx';
+import TaskForm from "../components/Tasks/TaskForm.tsx";
 
 
 const Home: React.FC = () =>  {
