acd579b0631256d45a21657cc03f5098a8f7ec75
stanleyavril123 <stanley.avril@outlook.com>
2024-12-17 21:51:00 -0500

    shared header

--- task-manager/src/App.tsx
+++ task-manager/src/App.tsx
@@ -4,10 +4,12 @@ import Home from './pages/Home.tsx';
 import Tasks from './pages/Tasks.tsx';
 import Pomodoro from './pages/Pomodoro.tsx';
 import Dashboard from './pages/Dashboard.tsx';
+import Header from './components/Header.tsx';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
       <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
--- task-manager/src/pages/Home.tsx
+++ task-manager/src/pages/Home.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import Header from '../components/Header.tsx'
+// import Header from '../components/Header.tsx'
 
 
 const Home: React.FC = () =>  {
   
   return (
 
-  <>
-    <Header/>
-  </>
+  // <>
+  //   <Header/>
+  // </>
+  <h1>Home page</h1>
 
 
 )}
