e091b2a219c8929e4613764954e6ee868c85f647
stanleyavril123 <stanley.avril@outlook.com>
2024-12-27 17:53:01 -0500

    added client side and server side

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import theme from "./styles/theme";
+import theme from "./styles/theme.js";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
 root.render(
   <ThemeProvider theme={theme}>
     <React.StrictMode>
