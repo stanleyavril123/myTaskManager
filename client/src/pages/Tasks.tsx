6f3b2b75ad1db124e306fa2aa1030ec8d81538a7
Stanley Avril <stanley.avril@outlook.com>
2025-01-22 13:17:01 -0500

    TODO

--- client/src/pages/Tasks.tsx
+++ client/src/pages/Tasks.tsx
@@ -4,6 +4,10 @@ import { Box } from "@mui/material";
 const Tasks: React.FC = () => {
   return (
     <Box sx={{ marginLeft: "80px" }}>
+      {/* 
+        TODO:
+        add a button that opens the TaskForum
+      */}
       <h1>Task Page</h1>
     </Box>
   );
